3cb15ed41d11fa13fad604b86b29f5bc6460042e
pratham kumar jha <prathamjha123dcs@gmail.com>
2024-09-26 16:56:02 +0530

    trying to fix unsplash api

--- app/api/unsplash/route.ts
+++ app/api/unsplash/route.ts
@@ -1,4 +1,4 @@
-import { NextRequest, NextResponse } from 'next/server';
+import { NextRequest, NextResponse} from "next/server";
 import axios from 'axios';
 // import { NextResponse } from 'next/server';
 
@@ -7,13 +7,14 @@ export  async function GET(req: NextRequest, res: NextResponse) {
     // Ensure the API key is defined
    
 const UNSPLASH_ACESS_KEY = process.env.UNSPLASH_ACESS_KEY
+const randomNum = Math.floor(Math.random() * 10000);
 console.log(UNSPLASH_ACESS_KEY);
-    const response = await axios.get(`https://api.unsplash.com/photos/random?count=9&orientation=landscape&client_id=${UNSPLASH_ACESS_KEY}`);
+    const response = await axios.get(`https://api.unsplash.com/photos/random?count=9&orientation=landscape&client_id=${UNSPLASH_ACESS_KEY}&random=${randomNum}`);
 
     if (!response) {
       throw new Error('Failed to fetch data');
     }
-        console.log(typeof response)
+        // console.log(typeof response)
     return  NextResponse.json(response.data); 
   } catch (error) {
     console.error('Error fetching data from Unsplash:', error);
