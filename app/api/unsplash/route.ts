666d847f601aad760ba425ab40d25033b3a3f667
pratham kumar jha <prathamjha123dcs@gmail.com>
2024-09-27 07:16:37 +0530

    handled caching on vercel

--- app/api/unsplash/route.ts
+++ app/api/unsplash/route.ts
@@ -1,23 +1,25 @@
-import { NextRequest, NextResponse} from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import axios from 'axios';
-// import { NextResponse } from 'next/server';
 
-export  async function GET(req: NextRequest, res: NextResponse) {
+export async function GET(req: NextRequest) {
   try {
     // Ensure the API key is defined
-   
-const UNSPLASH_ACESS_KEY = process.env.UNSPLASH_ACESS_KEY
-const randomNum = Math.floor(Math.random() * 10000);
-console.log(UNSPLASH_ACESS_KEY);
-    const response = await axios.get(`https://api.unsplash.com/photos/random?count=9&orientation=landscape&client_id=${UNSPLASH_ACESS_KEY}&random=${randomNum}`);
+    const UNSPLASH_ACESS_KEY = process.env.UNSPLASH_ACESS_KEY;
+    const randomNum = Math.floor(Math.random() * 10000);
 
-    if (!response) {
-      throw new Error('Failed to fetch data');
+    // Fetch data from Unsplash
+    const response = await fetch(`https://api.unsplash.com/photos/random?count=9&orientation=landscape&client_id=${UNSPLASH_ACESS_KEY}&random=${randomNum}`, { cache: 'no-store' });
+    
+    if (!response.ok) {
+      throw new Error('Unsplash API error');
     }
-        // console.log(typeof response)
-    return  NextResponse.json(response.data); 
+
+    const data = await response.json();
+
+    // Return the fetched data as JSON
+    return NextResponse.json(data);
   } catch (error) {
     console.error('Error fetching data from Unsplash:', error);
-    return new NextResponse("Internal error", { status: 500 }); 
+    return new NextResponse("Internal error", { status: 500 });
   }
 }
